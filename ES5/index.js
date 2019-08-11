2b2b47c3ff275ffe2822a033bbd171fdb8665319
jdamilasp <jdamilasp@gmail.com>
2019-08-11 13:50:23 +0530

    summary update in index.js

---
+++ ES5/index.js
@@ -0,0 +1,18 @@
+/**
+ * :: Summary 
+ * 1. Variable and Datatypes - string, number, boolean, null, undefined, object 
+ * 
+ * string -> string
+ * number -> number
+ * boolean -> number 
+ * null -> object 
+ * undefined -> undefined
+ * object -> object 
+ * array -> object 
+ * 
+ * 2. conditional - if else, swith 
+ * 3. loop -> for for..in while and do while 
+ * 4. Array method -> pop, push, shift, unshift 
+ * 5. Object and prototypes and this 
+ * 
+ */
